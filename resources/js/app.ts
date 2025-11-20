ce0fc288b75ac31d59a3d549f9f266f254650eeb
Said Abdulsalam <sabdulsalam@ubtconsults.se>
2025-11-20 15:18:10 +0100

    fix: ziggy

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,7 +4,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
 import vuetify from './plugins/vuetify';
 import Toast from './components/Toast.vue';
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -24,4 +25,10 @@ export default defineConfig({
             },
         })
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+            'ziggy-js': path.resolve(__dirname, './vendor/tightenco/ziggy'),
+        },
+    },
 });
